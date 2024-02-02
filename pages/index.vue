0ed12d00c56b028d7e3dfd55313039372eb26f27
adjonata <djonataalex@gmail.com>
2024-02-02 00:46:26 -0300

    feat: created basic seo

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,17 @@ definePageMeta({
   layout: "viewer",
 });
 useHead({
-  title: "Início - Mars View",
+  title: "Mars View",
+});
+
+const title = "Mars Viewer";
+const description =
+  'Bem-vindo ao "Mars Viewer", o seu portal exclusivo para explorar milhares de fotos fascinantes capturadas diariamente pelo nosso incansável rover em solo marciano.';
+useSeoMeta({
+  title: title,
+  ogTitle: title,
+  description: description,
+  ogDescription: description,
 });
 </script>
 
